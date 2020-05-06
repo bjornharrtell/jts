3bfe716b31e17cbe7182786fc838aabbe4c528cb
Björn Harrtell <bjorn@septima.dk>
2020-05-06 12:19:06 +0200

    Revert "Avoid getBoundary circular deps"
    This reverts commit 902059e942142b70bcd5dd3ed2bda14755a95ebd.

--- modules/core/src/main/java/org/locationtech/jts/geom/LineString.java
+++ modules/core/src/main/java/org/locationtech/jts/geom/LineString.java
@@ -12,6 +12,7 @@
 package org.locationtech.jts.geom;
 
 import org.locationtech.jts.algorithm.Length;
+import org.locationtech.jts.operation.BoundaryOp;
 
 /**
  *  Models an OGC-style <code>LineString</code>.
@@ -174,16 +175,7 @@ public class LineString
    * @see Geometry#getBoundary
    */
   public Geometry getBoundary() {
-    if (isEmpty()) {
-      return factory.createMultiPoint();
-    }
-    if (isClosed()) {
-        return factory.createMultiPoint();
-    }
-    return factory.createMultiPoint(new Point[]{
-                                     getStartPoint(),
-                                     getEndPoint()
-    });
+    return (new BoundaryOp(this)).getBoundary();
   }
 
   /**
--- modules/core/src/main/java/org/locationtech/jts/geom/MultiLineString.java
+++ modules/core/src/main/java/org/locationtech/jts/geom/MultiLineString.java
@@ -11,6 +11,7 @@
  */
 package org.locationtech.jts.geom;
 
+import org.locationtech.jts.operation.BoundaryOp;
 
 /**
  * Models a collection of {@link LineString}s.
@@ -79,6 +80,18 @@ public class MultiLineString
       }
     }
     return true;
+  }
+
+  /**
+   * Gets the boundary of this geometry.
+   * The boundary of a lineal geometry is always a zero-dimensional geometry (which may be empty).
+   *
+   * @return the boundary geometry
+   * @see Geometry#getBoundary
+   */
+  public Geometry getBoundary()
+  {
+    return (new BoundaryOp(this)).getBoundary();
   }
 
   /**
